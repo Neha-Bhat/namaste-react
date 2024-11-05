88c1e2c4839819bfdb90e9fd42a276d47dcc9016
Neha <nehabhat2209@gmail.com>
2024-11-05 23:10:17 +0530

    functional component contd..

--- app.js
+++ app.js
@@ -16,6 +16,29 @@ const heading = (
     <h1>React Element</h1>
 );
 
+const onereactEle = <span>One React Element</span>
+
+// React Element inside React Element, we can have functional component inside React Element too
+
+const MultilineHeadingComponent1 = () => (
+    <div id="container">
+        <HeadingComponent/>
+        {
+            console.log(number)
+        }
+        <h2>{number}</h2>
+        {heading}
+        <h1 className="heading">Namaste React another Multiline functional component</h1>
+    </div>
+)
+
+const reactEle = (
+    <h1>Hello
+        {onereactEle}
+        <MultilineHeadingComponent1/>
+    </h1>
+);
+
 // Functional Component
 // 3 ways for single line code
 // 1
@@ -40,6 +63,7 @@ const NormalFunction = function() {
 // below piece of code depicts multiline functional component
 // it also depicts nesting of components (Component Composition)
 // JS expression inside JSX
+// even React Element can be put inside JSX
 const number = 1000;
 const MultilineHeadingComponent = () => (
     <div id="container">
@@ -48,6 +72,8 @@ const MultilineHeadingComponent = () => (
             console.log(number)
         }
         <h2>{number}</h2>
+        {heading}
+        {reactEle}
         <h1 className="heading">Namaste React Multiline functional component</h1>
     </div>
 )
