7a8ad06545f60bc68f3a7fa68ff5da62a04c5182
Neha <nehabhat2209@gmail.com>
2024-11-07 23:04:01 +0530

    FDA: basic layout, header and card

--- app.js
+++ app.js
@@ -1,85 +1,52 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-// const headingReact = React.createElement("h1", {id: "headingId"}, "Hello from React"); //returns an element - a JS object
-// const rooot = ReactDOM.createRoot(document.getElementById("root"));
-// rooot.render(headingReact);
-
-// JSX
-const jsxHeading = <h1 id="headingId">Namaste React in JSX</h1>
-console.log(jsxHeading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
-
-// React Element
-const heading = (
-    <h1>React Element</h1>
-);
-
-const onereactEle = <span>One React Element</span>
-
-// React Element inside React Element, we can have functional component inside React Element too
-
-const MultilineHeadingComponent1 = () => (
-    <div id="container">
-        <HeadingComponent/>
-        {
-            console.log(number)
-        }
-        <h2>{number}</h2>
-        {heading}
-        <h1 className="heading">Namaste React another Multiline functional component</h1>
-    </div>
-)
-
-const reactEle = (
-    <h1>Hello
-        {onereactEle}
-        <MultilineHeadingComponent1/>
-    </h1>
-);
-
-// Functional Component
-// 3 ways for single line code
-// 1
-const HeadingComponent = () => {
-    return <h1 className="head">Namaste React Functional Component</h1>;
-}
-//2
-const HeadingComponent1 = () => (
-    <h1>Type 2 heading</h1>
-)
-//3
-const HeadingComponent2 = () => <h1>Type 3</h1>
-
-// We can use normal functions too
-const NormalFunction = function() {
+const Header = () => {
     return (
-        <h1>Normal Function</h1>
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://th.bing.com/th/id/OIP.uZ2JtS-P27KGz_YIX8lTgQAAAA?w=161&h=180&c=7&r=0&o=5&pid=1.7"/>
+            </div>
+            <div className="nav-container">
+                <ul className="nav-items">
+                    <li>Home</li>
+                    <li>Contact Us</li>
+                    <li>About Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
+const ResCard = () => {
+    return (
+        <div className="res-card">
+            <img className="food-image" src="https://vinayasculinarydelights.com/wp-content/uploads/2020/11/IMG_20201128_111556__01-03.jpeg" />
+            <h3>Vidyarthi Bhavan</h3>
+        </div>
+    )
+}
 
-// below piece of code depicts multiline functional component
-// it also depicts nesting of components (Component Composition)
-// JS expression inside JSX
-// even React Element can be put inside JSX
-const number = 1000;
-const MultilineHeadingComponent = () => (
-    <div id="container">
-        <HeadingComponent/>
-        {/* also can be written as */}
-        <HeadingComponent></HeadingComponent>
-        {/* also can be written as */}
-        {HeadingComponent()}
-        {
-            console.log(number)
-        }
-        <h2>{number}</h2>
-        {heading}
-        {reactEle}
-        <h1 className="heading">Namaste React Multiline functional component</h1>
-    </div>
-)
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <ResCard />
+            </div>
+        </div>
+    )
+}
 
-root.render(<MultilineHeadingComponent/>);
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+Root.render(<AppLayout />)
