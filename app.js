988900e272c7a32eddc8e63c187c3ec990ea1d16
Neha <nehabhat2209@gmail.com>
2024-11-13 17:03:00 +0530

    adding props for dynamic data

--- app.js
+++ app.js
@@ -19,18 +19,26 @@ const Header = () => {
     )
 }
 
-const ResCard = () => {
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span><i>Namaste Food</i></span>
+        </div>
+    )
+};
+
+const ResCard = (props) => {
     return (
         <div className="res-card">
-            <img className="food-image" src="https://b.zmtcdn.com/data/pictures/chains/0/18924790/11622036dde0d49cc0dc77cf266983ff_o2_featured_v2.jpg" />
+            <img className="food-image" src={props.thumbnail} />
             <div className="res-info">
-                <h3>Namaste</h3>
-                <span className="rating">4.3</span>
+                <h3>{props.resName}</h3>
+                <span className="rating">{props.rating}</span>
             </div>
             
-            <h4>South Indian</h4>
-            <h4>INR 100 for one</h4>
-            <b>26 min</b>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.price}</h4>
+            <b>{props.eta}</b>
         </div>
     )
 }
@@ -40,13 +48,8 @@ const Body = () => {
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <ResCard />
-                <ResCard />
-                <ResCard />
-                <ResCard />
-                <ResCard />
-                <ResCard />
-                <ResCard />
+                <ResCard resName="Namaste" cuisine="South Indian" thumbnail="https://b.zmtcdn.com/data/pictures/chains/0/18924790/11622036dde0d49cc0dc77cf266983ff_o2_featured_v2.jpg" rating="4.3" price="INR 100 for one" eta="26 min"/>
+                <ResCard resName="Ibaco" cuisine="Desserts, Ice cream" thumbnail="https://b.zmtcdn.com/data/pictures/chains/8/53198/995145af92d5d5004cc7e180d4d104c1_o2_featured_v2.jpg" rating="4.0" price="INR 150 for one" eta="20 min"/>
             </div>
         </div>
     )
@@ -57,6 +60,7 @@ const AppLayout = () => {
         <div className="app">
             <Header />
             <Body />
+            <Footer />
         </div>
     )
 }
