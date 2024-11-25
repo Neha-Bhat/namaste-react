9ca3eecb4216a37c077fdd6b7e3710d205d0493b
Neha <nehabhat2209@gmail.com>
2024-11-25 18:10:03 +0530

    dynamic data

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,8 @@ import resData from "../utils/mockData";
 const Body = () => {
     return (
         <div className="body">
-            <div className="search">Search</div>
+            {/* <div className="search">Search</div> */}
+            <button className="filter" onClick={() => {resData = filterRestaurants()}}>Top Restaurants</button>
             <div className="res-container">
                 {
                     resData.map(restaurant => <ResCard key={restaurant.info.id} resData={restaurant}/>)
@@ -12,6 +13,10 @@ const Body = () => {
             </div>
         </div>
     )
+}
+
+function filterRestaurants() {
+    return resData.filter(res => res.info.avgRating >=4.5)
 }
 
 export default Body;
