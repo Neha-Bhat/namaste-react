80e4a68101969715bb6de589b004f0fc4e70ff1a
Neha <nehabhat2209@gmail.com>
2024-12-04 17:46:25 +0530

    Hooks: useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,23 @@
 import ResCard from "./ResCard";
 import resData from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
     let [listOfRestaurants, setListOfRestaurants] = useState(resData);
-    // const listOfRestaurants = [];
+    // useEffect will be executed after the component is rendered
+    useEffect(() => {
+        console.log("useEffect called")
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.zomato.com/webroutes/search/home"); // add Swiggy API URL here
+        const json = data.json();
+        console.log(json)
+    }
+
+    console.log("body called")
+
     return (
         <div className="body">
             {/* <div className="search">Search</div> */}
