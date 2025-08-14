fce0c1fa4b02449f80d0213fb09651c4703530af
Neha <nehabhat2209@gmail.com>
2025-08-14 17:45:06 +0530

    lazily loaded ResMenu

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy } from "react";
 import ReactDOM  from "react-dom/client";
 import Header from './components/Header';
 import Footer from "./components/Footer";
@@ -7,11 +7,13 @@ import About from "./components/About";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import RestaurantMenu from "./components/RestaurantMenu";
+// import RestaurantMenu from "./components/RestaurantMenu";
 
 function truncateText(str, maxLength) {
   return  str.length > maxLength ? str.slice(0, maxLength)+('...') : str
 }
+
+const RestaurantMenu = lazy(() => import("./components/RestaurantMenu")); //lazy loading
 
 const AppLayout = () => {
     return (
